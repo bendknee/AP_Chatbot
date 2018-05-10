f83fc03eec708e68a02084023a45a6bb26992e70
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-05-11 03:03:45 +0700

    Created basic test to check command availability (#32)

---
+++ src/test/java/advprog/example/bot/controller/RecentTweetsControllerTest.java
@@ -0,0 +1,43 @@
+package advprog.example.bot.controller;
+
+import advprog.example.bot.EventTestUtil;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+public class RecentTweetsControllerTest {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Autowired
+    private EchoController echoController;
+
+    @Test
+    void testHandleTextMessageEvent() {
+        // TheObsessivePr1 is a dummy Twitter account with more than 5 tweets
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("/tweet recent TheObsessivePr1");
+
+        TextMessage reply = echoController.handleTextMessageEvent(event);
+        String[] arrayOfTweets = reply.getText().split("\n");
+
+        assertEquals(5, arrayOfTweets.length);
+    }
+}
