5f26a83647cc8c1b75b58d74cea3f3d1565457a7
zulia <zulia@Zulias-MacBook-Pro.local>
2018-05-11 19:20:44 +0700

    test

--- src/main/java/advprog/example/bot/country/BillboardCountry.java
+++ src/main/java/advprog/example/bot/country/BillboardCountry.java
@@ -19,6 +19,45 @@ public class BillboardCountry {
     public BillboardCountry(String billboardUrl) {
         this.billboardUrl = billboardUrl;
         topTenCountryList = new ArrayList<Song>();
+        setTopTenCountrySongs(billboardUrl);
     }
 
+    private void setTopTenCountrySongs(String url) {
+        try {
+            Document docs = Jsoup.connect(url).get();
+            Elements links = docs.getElementsByClass("chart-row");
+            for (int i = 0; i < 10; i++) {
+                Element elem = links.get(i);
+                String name = elem.getElementsByClass("chart-row_song").html();
+                String artist = elem.getElementsByClass("chart-row__artis").html();
+
+                String formattedNames = Parser.unescapeEntities(name, false);
+                String formattedArtists = Parser.unescapeEntities(artist, false);
+
+                Song song = new Song(formattedNames, formattedArtists);
+                topTenCountryList.add(song);
+
+            }
+        } catch (IOException e) {
+            System.out.println("Illegal IO");
+        }
+    }
+
+    public String printTopTentList() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int counter = 1;
+        for (Song s: topTenCountryList) {
+            stringBuilder.append(format("(%d) %s\n", counter, s.toString()));
+            counter++;
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getBillboardUrl() {
+        return billboardUrl;
+    }
+
+    public List<Song> getTopTenCountryList() {
+        return topTenCountryList;
+    }
 }
