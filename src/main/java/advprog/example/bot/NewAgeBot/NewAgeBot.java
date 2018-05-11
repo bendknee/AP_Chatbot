c59e46362f98763aa408d34a7ec25a6018dbecd4
Khatya <khatyaputeri@gmail.com>
2018-05-11 18:52:43 +0700

    merapihkan ke dalam package

--- src/main/java/advprog/example/bot/NewAgeBot/NewAgeBot.java
+++ src/main/java/advprog/example/bot/NewAgeBot/NewAgeBot.java
@@ -1,16 +1,18 @@
-package advprog.example.bot;
-
+package advprog.example.bot.NewAgeBot;
 
 import java.util.HashMap;
 
 public class NewAgeBot {
+
+    HashMap<Integer, String> chart;
+    String artist;
 
     public HashMap<Integer, String> getChart(){
 
     }
 
     public boolean isExist() {
-
+        for(int i = 0; i< )
     }
 
     public HashMap<Integer, String> getAllArtis() {
