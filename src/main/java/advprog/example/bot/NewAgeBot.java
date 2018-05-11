f55bee949d3ab569d302664a1ab3223c6f20f052
Khatya <khatyaputeri@gmail.com>
2018-05-11 11:21:54 +0700

    setUp class, setUp test

---
+++ src/main/java/advprog/example/bot/NewAgeBot.java
@@ -0,0 +1,22 @@
+package advprog.example.bot;
+
+
+import java.util.HashMap;
+
+public class NewAgeBot {
+
+    public HashMap<Integer, String> getChart(){
+
+    }
+
+    public boolean isExist() {
+
+    }
+
+    public HashMap<Integer, String> getAllArtis() {
+
+    }
+
+
+
+}
---
+++ src/test/java/advprog/example/bot/NewAgeTest.java
@@ -0,0 +1,26 @@
+package advprog.example.bot;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class NewAgeTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testIsExistFunction() {
+
+    }
+
+    @Test
+    public void testGetChart() {
+
+    }
+
+    //TODO apakah menggunakan api yang benar
+
+}
