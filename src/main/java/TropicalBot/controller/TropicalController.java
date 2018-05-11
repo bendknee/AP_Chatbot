116e64a9d68c25cedab316d8d71cdc680b038359
Winston Chandra <winsgates@yahoo.com>
2018-05-12 01:09:01 +0700

    B2 20 minor changes on tropicalController

--- src/main/java/TropicalBot/controller/TropicalController.java
+++ src/main/java/TropicalBot/controller/TropicalController.java
@@ -37,7 +37,7 @@ public class TropicalController {
             return new TextMessage("Sorry, Artist "+ content.getText()+ " is not in the chart" );
         }
         String result = cekArtis(artist);
-        if (result == null) {
+        if (result == "") {
             return new TextMessage("Sorry, Artist "+ content.getText()+ " is not in the chart" );
         }
         return new TextMessage(result);
@@ -52,11 +52,11 @@ public class TropicalController {
     @EventMapping
     public static String cekArtis(String artis) throws IOException {
         Document doc = Jsoup.connect("https://www.billboard.com/charts/tropical-songs").get();
-        Elements containers = doc.select(".chart-row__artist");
+        Elements containers = doc.select(".chart-row__title");
         String hasil = "";
         for (int i = 0; i < 25; i++) {
             Element elements = containers.get(i);
-            if (elements.text().equalsIgnoreCase(artis)) {
+            if (elements.select(".chart-row__artist").text().equalsIgnoreCase(artis)) {
                 hasil += "\n"+elements.select(".chart-row__artist").text() + "\n" +
                         elements.select(".chart-row__song").text() + "\n" + "Position : " + (i + 1) + "\n";
             }
