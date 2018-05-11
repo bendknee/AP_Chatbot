8e594918a8e4e5f5c4d7f78812d3617143054ead
Nathan <markusk123@live.com>
2018-05-11 11:31:14 +0700

    create oriconcomicrankingcontroller.java skeleteon file

---
+++ src/main/java/advprog/example/bot/controller/OriconComicRankingController.java
@@ -0,0 +1,24 @@
+package advprog.example.bot.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+
+import java.util.logging.Logger;
+
+public class OriconComicRankingController {
+    private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        // TODO : Implement this
+        return null;
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        // TODO : Implement this
+    }
+}
