dead34ea07a316251aebe9e566eba1bc1a1f3c04
Winston Chandra <winsgates@yahoo.com>
2018-05-11 16:57:40 +0700

    First Commit

--- src/main/java/BillboardPackage/controller/BillboardController.java
+++ src/main/java/BillboardPackage/controller/BillboardController.java
@@ -1,13 +1,57 @@
 package BillboardPackage.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
 public class BillboardController {
+    private static final Logger LOGGER = Logger.getLogger(BillboardController.class.getName());
 
+    private static final RestTemplate restTemplate = new RestTemplate();
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String textContext = content.getText();
+        String parser = textContext.substring(0,18);
+        String artist = textContext.substring(19,textContext.length());
+        try {
+            if (!parser.equals("/billboard bill200 ")) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            return new TextMessage("Command not found for " + content.getText());
+        }
+        String result = cekArtis(artist);
+        if (result == null) {
+            return new TextMessage("Command not found for " + content.getText());
+        }
+        return new TextMessage(result);
+    }
+
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
+
+    @EventMapping
     public static String cekArtis(String artis) throws IOException {
         Document doc = Jsoup.connect("https://www.billboard.com/charts/billboard-200").get();
         Elements containers = doc.select(".chart-row__artist");
@@ -15,8 +59,8 @@ public class BillboardController {
         for (int i = 0; i < 200; i++) {
             Element elements = containers.get(i);
             if (elements.text().equalsIgnoreCase(artis)) {
-                hasil += elements.select(".chart-row__artist").text()+"\n"+
-                        elements.select(".chart-row__song").text()+"\n" +"Position : "+(i+1)+"\n";
+                hasil += "\n"+elements.select(".chart-row__artist").text() + "\n" +
+                        elements.select(".chart-row__song").text() + "\n" + "Position : " + (i + 1) + "\n";
             }
         }
         return hasil;
