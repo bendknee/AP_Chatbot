40f4986835b39822f7cd3fe1425b5b4ab2d2bfa2
Endrawan Andika Wicaksana <aendrawan@yahoo.co.id>
2018-05-14 01:22:09 +0700

    implement tied percentage and "" in empty rank slot

--- src/main/java/advprog/example/bot/laughers/LaughersManager.java
+++ src/main/java/advprog/example/bot/laughers/LaughersManager.java
@@ -48,52 +48,64 @@ public class LaughersManager {
 
         long sumOfAllLaugh = laughersList.stream().mapToLong(Laughers::getNumberOfLaugh).sum();
 
+        int[] percentage = new int[5];
         for (int i = 0; i < Math.min(5, laughersList.size()); i++) {
+            Laughers laughers = laughersList.get(i);
+            percentage[i] = (int) ((double) laughers.getNumberOfLaugh() * 100.0 /
+                (double) sumOfAllLaugh);
+        }
+
+        for (int i = 0; i < 5; i++) {
             if (i != 0) {
                 stringBuilder.append("\n");
             }
 
-            Laughers laughers = laughersList.get(i);
+            if (i < laughersList.size()) {
+                Laughers laughers = laughersList.get(i);
 
-            stringBuilder.append(i + 1);
-            stringBuilder.append(". ");
-
-            try {
-                switch (laughers.getGroupId().charAt(0)) {
-                    case 'C':
-                        stringBuilder.append(lineMessagingClient
-                                                 .getGroupMemberProfile(groupId,
-                                                                        laughers.getUserId())
-                                                 .get().getDisplayName());
-                        break;
-                    case 'R':
-                        stringBuilder.append(lineMessagingClient
-                                                 .getRoomMemberProfile(groupId,
-                                                                       laughers.getUserId())
-                                                 .get().getDisplayName());
-                        break;
-                    case 'U':
-                    default:
-                        stringBuilder.append(lineMessagingClient
-                                                 .getProfile(laughers.getUserId())
-                                                 .get().getDisplayName());
-                        break;
+                int rank = i;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (percentage[i] == percentage[j]) {
+                        rank = j;
+                    }
                 }
-            } catch (Exception e) {
-                return "Error when get user profile name";
+                stringBuilder.append(rank + 1);
+                stringBuilder.append(". ");
+
+                try {
+                    switch (laughers.getGroupId().charAt(0)) {
+                        case 'C':
+                            stringBuilder.append(lineMessagingClient
+                                                     .getGroupMemberProfile(groupId,
+                                                                            laughers.getUserId())
+                                                     .get().getDisplayName());
+                            break;
+                        case 'R':
+                            stringBuilder.append(lineMessagingClient
+                                                     .getRoomMemberProfile(groupId,
+                                                                           laughers.getUserId())
+                                                     .get().getDisplayName());
+                            break;
+                        case 'U':
+                        default:
+                            stringBuilder.append(lineMessagingClient
+                                                     .getProfile(laughers.getUserId())
+                                                     .get().getDisplayName());
+                            break;
+                    }
+                } catch (Exception e) {
+                    return "Error when get user profile name";
+                }
+
+                stringBuilder.append(" (");
+                stringBuilder.append(percentage[i]);
+                stringBuilder.append("%)");
+            } else {
+                stringBuilder.append(i + 1);
+                stringBuilder.append(". ");
             }
-
-            stringBuilder.append(" (");
-            stringBuilder.append((int) ((double) laughers.getNumberOfLaugh() * 100.0 /
-                (double) sumOfAllLaugh));
-            stringBuilder.append("%)");
         }
 
-        String result = stringBuilder.toString();
-        if (result.equals("")) {
-            result = "Tidak ada yang ketawa";
-        }
-
-        return result;
+        return stringBuilder.toString();
     }
 }
