279e66e29f82072a6e612614b501987f036fcd07
Benny William Pardede <bpardedewilliam@gmail.com>
2018-05-14 15:56:18 +0700

    Fully Implemented all requirements. Not tested yet.

--- src/main/java/advprog/example/bot/controller/NearbyPhotosController.java
+++ src/main/java/advprog/example/bot/controller/NearbyPhotosController.java
@@ -1,54 +1,122 @@
 package advprog.example.bot.controller;
 
-import com.aetrion.flickr.Flickr;
-import com.aetrion.flickr.REST;
-import com.aetrion.flickr.test.TestInterface;
 import com.linecorp.bot.model.action.URIAction;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.LocationMessageContent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.ImageMessage;
-import com.aetrion.flickr.places.Place;
 import com.linecorp.bot.model.message.template.ImageCarouselColumn;
 import com.linecorp.bot.model.message.template.ImageCarouselTemplate;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @LineMessageHandler
 public class NearbyPhotosController {
 
     private static final Logger LOGGER = Logger.getLogger(NearbyPhotosController.class.getName());
-    private Flickr flickr;
+    private final String BASE_URL = "https://api.flickr.com/services/rest/?method=";
+    private final String API_KEY = "4847f0e678f60a5f7e213521c263deef";
+    private final String API_METHOD = "flickr.photos.search";
+    private final String EXTENSION_PARAM = "&per_page=5&format=json&nojsoncallback=1";
+    private HashSet<String> requests;
 
-
-    public NearbyPhotosController() throws ParserConfigurationException {
-        String apiKey = "4847f0e678f60a5f7e213521c263deef ";
-        String sharedSecret = "4189ad7a7e127c70";
-        flickr = new Flickr(apiKey, sharedSecret, new REST());
-    }
-
-    @EventMapping
-    public ImageCarouselTemplate handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
+    /*@EventMapping
+    public TextMessage handleApplicationTrigger(MessageEvent<TextMessageContent> event) {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
 
-        /*
-        * implement line Location getter API
-        * implement Flickr GeoPhotosbyLocation API
-        * implement Line image message postman
-        */
+
+    }*/
+
+    @EventMapping
+    public ImageCarouselTemplate handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
+        LOGGER.fine(String.format("LocationMessageContent(timestamp='%s',title='%s')",
+                event.getTimestamp(), event.getMessage().getTitle()));
 
         LocationMessageContent content = event.getMessage();
-        // -6.364546
-        // 106.828611
-        new ImageCarouselColumn("wdw", new URIAction("","URI"));
-        ImageCarouselTemplate carousel = new ImageCarouselTemplate(null);
+        String latitude = Double.toString(content.getLatitude());
+        String longitude = Double.toString(content.getLongitude());
 
-        return carousel;
+        String flickrRestUrl =  BASE_URL + API_METHOD + "&api_key=" + API_KEY + "&lat="
+                + latitude + "&lon=" + longitude + EXTENSION_PARAM;
+
+        String stringifiedJson = restGetMethod(flickrRestUrl);
+        List<Map<String, String>> allPhotosData = stringToJson(stringifiedJson);
+        List<ImageCarouselColumn> carouselColumns = carouselColumnsGenerator(allPhotosData);
+
+        return new ImageCarouselTemplate(carouselColumns);
+    }
+
+    private String restGetMethod(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders header = new HttpHeaders();
+        //HttpEntity<String> entity = new HttpEntity<>("parameters", header);
+        return restTemplate.getForObject(url, String.class, header);
+    }
+
+    private List<Map<String, String>> stringToJson(String string) {
+        try {
+            List<Map<String, String>> allObjects = new ArrayList<>();
+            JSONObject parsedJson = new JSONObject(string);
+            JSONArray photos = parsedJson.getJSONObject("photos").getJSONArray("photo");
+
+            for (int i = 0; i < photos.length(); ++i) {
+                JSONObject singlePhoto = photos.getJSONObject(i);
+                allObjects.add(customJsonAttributeGet(singlePhoto));
+            }
+            return allObjects;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private List<ImageCarouselColumn> carouselColumnsGenerator(List<Map<String, String>> allImages) {
+        ArrayList<ImageCarouselColumn> carouselColumns = new ArrayList<>();
+        for (Map<String, String> image : allImages) {
+            String flickrSourceUrl = String.format(
+                    "https://farm%s.staticflickr.com/%s/%s_%s.jpg", image.get("farm"),
+                    image.get("server"), image.get("id"), image.get("secret"));
+            String flickrWebUrl = String.format(
+                    "https://www.flickr.com/photos/%s/%s", image.get("owner"), image.get("id")
+            );
+            String title;
+            if (image.get("title").length() > 12) {
+                title = image.get("title").substring(0,9) + "...";
+            } else {
+                title = image.get("title");
+            }
+            carouselColumns.add(new ImageCarouselColumn(flickrSourceUrl, new URIAction(title, flickrWebUrl)));
+        }
+        return carouselColumns;
+    }
+
+    private Map<String, String> customJsonAttributeGet(JSONObject jsonObject) {
+        try {
+            HashMap<String, String> objectMap = new HashMap<>();
+
+            objectMap.put("id", jsonObject.getString("id"));
+            objectMap.put("secret", jsonObject.getString("secret"));
+            objectMap.put("server", jsonObject.getString("server"));
+            objectMap.put("farm", jsonObject.getString("farm"));
+            objectMap.put("title", jsonObject.getString("title"));
+            objectMap.put("owner", jsonObject.getString("owner"));
+
+            return objectMap;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
