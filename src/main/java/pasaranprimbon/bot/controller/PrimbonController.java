854026c36cc1d1b9864028b2f70bbe95cfe64d5f
bagasirwansyah <bagasirwansyah@yahoo.com>
2018-05-15 00:56:19 +0700

    Serialize warning

--- src/main/java/pasaranprimbon/bot/controller/PrimbonController.java
+++ src/main/java/pasaranprimbon/bot/controller/PrimbonController.java
@@ -15,6 +15,7 @@ import java.util.logging.Logger;
 @LineMessageHandler
 public class PrimbonController {
 
+    private static final long serialVersionUID = 7526471155622776147L;
     private static final Logger LOGGER = Logger.getLogger(advprog.example.bot.controller.EchoController.class.getName());
 
     @EventMapping
