db1b8685a4c4aecbce995cf8bbe1ed421a5d2e5c
arkha98 <arkhasayoga.m@gmail.com>
2018-05-16 05:06:34 +0700

    do some scrapping

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -6,8 +6,16 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
+
+import static java.lang.String.format;
 
 @LineMessageHandler
 public class EchoController {
@@ -34,6 +42,44 @@ public class EchoController {
     @EventMapping
     public void handleTextContentPublic (String replyToken, Event event, TextMessage content) throws Exception {
         handleTextContent(replyToken, event, content);
+    }
+
+    @EventMapping
+    private ArrayList<String> scrapping(String mode, String date) throws IOException {
+        String offset = "";
+        ArrayList<String> val = new ArrayList<>();
+        if (mode.toLowerCase().equals("daily")) {
+            offset = "https://www.oricon.co.jp/rank/bd/d/"+date+"/";
+        }
+        else if (mode.toLowerCase().equals("weekly")) {
+            offset = "https://www.oricon.co.jp/rank/bd/w/"+date+"/";
+        }
+        else {
+            val.clear();
+            val.add("Input Mode Salah!!!\n\n\nFormat input\n/oricon bluray <weekly|daily> YYYY-MM-DD");
+            return val;
+        }
+        try {
+            Document dc = Jsoup.connect(offset).timeout(3000).get();
+            Elements body = dc.select("section.box-rank-entry");
+
+            int i = 1;
+            for (Element rank : body) {
+                String title = rank.select("h2.title").text();
+                String artist = rank.select("p.name").text();
+                Elements list = rank.select("ul.list li");
+                String release = list.get(0).text();
+                release = release.substring(5, 9) + "-" + release.substring(10, 12) + "-" + release.substring(13, 15);
+                val.add(format("(%d) %s - %s - %s", i, title, artist, release));
+                i++;
+            }
+            return val;
+        }
+        catch (IOException e) {
+            val.clear();
+            val.add("Input tanggal salah atau tidak ditemukan!!!\n\n\nFormat input\n/oricon bluray <weekly|daily> YYYY-MM-DD");
+            return val;
+        }
     }
 
     @EventMapping
