30d03c41c5da25b56806fccf97fd24821e69ba85
Raden Fikri <radenfikri12@gmail.com>
2018-05-16 16:06:57 +0700

    test

--- src/test/java/advprog/example/bot/controller/YoutubeInfoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/YoutubeInfoControllerTest.java
@@ -23,7 +23,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
-public class EchoControllerTest {
+public class YoutubeInfoControllerTest {
 
     static {
         System.setProperty("line.bot.channelSecret", "SECRET");
@@ -31,11 +31,11 @@ public class EchoControllerTest {
     }
 
     @Autowired
-    private EchoController echoController;
+    private YoutubeInfoController youtubeInfoController;
 
     @Test
     void testContextLoads() {
-        assertNotNull(echoController);
+        assertNotNull(youtubeInfoController);
     }
 
     @Test
@@ -43,7 +43,7 @@ public class EchoControllerTest {
         MessageEvent<TextMessageContent> event =
                 EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
 
-        TextMessage reply = echoController.handleTextMessageEvent(event);
+        TextMessage reply = youtubeInfoController.handleTextMessageEvent(event);
 
         assertEquals("", reply.getText());
     }
