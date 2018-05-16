207c7f13f4a5a7b6f090778bf17687d8125c2abc
Winston Chandra <winsgates@yahoo.com>
2018-05-16 15:19:14 +0700

    B2 53 adding some testcases and init some stubs

--- src/main/java/botnewrelease/controller/NewReleaseController.java
+++ src/main/java/botnewrelease/controller/NewReleaseController.java
@@ -1,4 +1,32 @@
 package botnewrelease.controller;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.util.logging.Logger;
+
+@LineMessageHandler
 public class NewReleaseController {
+
+    private static final Logger LOGGER = Logger.getLogger(NewReleaseController.class.getName());
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String contentText = content.getText();
+        /* To do method process (Stub)*/
+        return new TextMessage(contentText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
 }
