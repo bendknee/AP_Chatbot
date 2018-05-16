1c2a09cafba90b9507c8c9f7304dd1e348304afb
Benny William Pardede <bpardedewilliam@gmail.com>
2018-05-16 11:28:25 +0700

    Rearranged Project Directory

--- src/test/java/advprog/flickr/bot/controller/NearbyPhotosControllerTest.java
+++ src/test/java/advprog/flickr/bot/controller/NearbyPhotosControllerTest.java
@@ -1,4 +1,4 @@
-package advprog.example.bot.controller;
+package advprog.flickr.bot.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
