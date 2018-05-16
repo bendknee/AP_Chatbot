2fe5edb770ac860e8f7ded53b8e5314801802500
Benny William Pardede <bpardedewilliam@gmail.com>
2018-05-16 10:20:39 +0700

    Inserted unit tests for Message Location Handler

--- src/test/java/advprog/example/bot/EventTestUtil.java
+++ src/test/java/advprog/example/bot/EventTestUtil.java
@@ -1,7 +1,9 @@
 package advprog.example.bot;
 
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.UnknownSource;
 import com.linecorp.bot.model.event.source.UserSource;
 
 import java.time.Instant;
@@ -16,5 +18,12 @@ public class EventTestUtil {
         return new MessageEvent<>("replyToken", new UserSource("userId"),
                 new TextMessageContent("id", text),
                 Instant.parse("2018-01-01T00:00:00.000Z"));
+    }
+
+    public static MessageEvent<LocationMessageContent> createDummyLocationMessage(
+            double latitude, double longitude) {
+        return new MessageEvent<>("replyToken", new UnknownSource(),
+                new LocationMessageContent("id", "title", "address",
+                        latitude, longitude), Instant.parse("2018-01-01T00:00:00.000Z"));
     }
 }
--- src/test/java/advprog/example/bot/controller/NearbyPhotosControllerTest.java
+++ src/test/java/advprog/example/bot/controller/NearbyPhotosControllerTest.java
@@ -2,22 +2,23 @@ package advprog.example.bot.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
+import advprog.example.bot.EventTestUtil;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.LocationMessageContent;
-import com.linecorp.bot.model.event.source.UnknownSource;
-import com.linecorp.bot.model.message.ImageMessage;
-
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.message.template.ImageCarouselTemplate;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
@@ -32,19 +33,38 @@ public class NearbyPhotosControllerTest {
     private NearbyPhotosController nearbyPhotosController;
 
     @Test
-    void testContextLoads() {
+    public void testContextLoads() {
         assertNotNull(nearbyPhotosController);
     }
 
     @Test
-    void testHandleLocationMessageEvent() {
-        MessageEvent<LocationMessageContent> event = new MessageEvent<LocationMessageContent>("4d0sah2z",
-                new UnknownSource(), new LocationMessageContent("id69",
-                "Fakultas Ilmu Komputer", "Kukusan Beji 14045",
-                -6.364546, 106.828611), null);
+    public void testUrbanAreaLocationMessage() {
+        MessageEvent<LocationMessageContent> event =
+                EventTestUtil.createDummyLocationMessage(-6.369084, 106.832578);
+        List<Message> responseMessage = nearbyPhotosController.handleLocationMessageEvent(event);
+        assertTrue(responseMessage.size() > 0);
+        TemplateMessage message = (TemplateMessage) responseMessage.get(0);
+        ImageCarouselTemplate carousel = (ImageCarouselTemplate) message.getTemplate();
+        assertTrue(carousel.getColumns().size() > 0);
+    }
 
-        ImageCarouselTemplate response = nearbyPhotosController.handleLocationMessageEvent(event);
+    @Test
+    public void testRuralAreaLocationMessage() {
+        MessageEvent<LocationMessageContent> event =
+                EventTestUtil.createDummyLocationMessage(-6.160657, 107.423090);
+        List<Message> responseMessage = nearbyPhotosController.handleLocationMessageEvent(event);
+        assertTrue(responseMessage.size() > 0);
+        TextMessage message = (TextMessage) responseMessage.get(0);
+        assertEquals(message.getText(), "No image found. Perhaps select a more populous location.");
+    }
 
-        assertEquals(response.getColumns().size(), 5);
+    @Test
+    public void testKeywordMessageTrigger() {
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("nearby photos");
+        List<TextMessage> responseMessage = nearbyPhotosController.handleKeywordTrigger(event);
+        assertTrue(responseMessage.size() > 0);
+        TextMessage textMessage = responseMessage.get(0);
+        assertTrue(textMessage.getText().contains("'Share location' feature below"));
     }
 }
