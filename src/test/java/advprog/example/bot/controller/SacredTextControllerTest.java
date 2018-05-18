379e70550985547aa4a3b160d6aa86bee64b9217
Nathan <markusk123@live.com>
2018-05-18 20:18:50 +0700

    create STUB for sacredtextcontroller and sacredtextcontrollertest files

---
+++ src/test/java/advprog/example/bot/controller/SacredTextControllerTest.java
@@ -0,0 +1,59 @@
+package advprog.example.bot.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+import advprog.example.bot.EventTestUtil;
+import junit.framework.TestCase;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+public class SacredTextControllerTest extends TestCase {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Autowired
+    public SacredTextController sacredTextController = new SacredTextController();
+
+    @Test
+    public void testContextLoads() {
+        assertNotNull(sacredTextController);
+    }
+
+    @Test
+    public void testHandleTextMessageEvent() {
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("/sacred Lorem Ipsum");
+
+        TextMessage reply = sacredTextController.handleTextMessageEvent(event);
+
+        assertEquals("sacred~ Lorem Ipsum", reply.getText());
+    }
+
+    @Test
+    public void testHandleDefaultMessage() {
+        Event event = mock(Event.class);
+
+        sacredTextController.handleDefaultMessage(event);
+
+        verify(event, atLeastOnce()).getSource();
+        verify(event, atLeastOnce()).getTimestamp();
+    }
+}
