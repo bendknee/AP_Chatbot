602ae7395104b6697684590a0e85da3506f195f9
Winston Chandra <winsgates@yahoo.com>
2018-05-20 05:02:05 +0700

    Fixing testcase B2 53

--- src/test/java/newreleasetest/controller/NewReleaseTester.java
+++ src/test/java/newreleasetest/controller/NewReleaseTester.java
@@ -26,10 +26,9 @@ import java.io.IOException;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
 public class NewReleaseTester {
 
