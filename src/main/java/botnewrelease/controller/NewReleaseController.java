6184490d48314eb59a0e955755008f88be89c1b0
Winston Chandra <winsgates@yahoo.com>
2018-05-20 21:55:53 +0700

    B2 53 fix

--- src/main/java/botnewrelease/controller/NewReleaseController.java
+++ src/main/java/botnewrelease/controller/NewReleaseController.java
@@ -65,7 +65,7 @@ public class NewReleaseController {
                 contentText.length() > 22) {
             throw new IllegalArgumentException();
         }
-        return new TextMessage(cekNewRelease().substring(0, 2000));
+        return new TextMessage(cekNewRelease().substring(0, 2030));
     }
 
     @EventMapping
