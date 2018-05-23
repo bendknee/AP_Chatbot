18c6d4a3354ce54ae87a4155de0fa01f956f208f
Khatya <khatyaputeri@gmail.com>
2018-05-23 16:06:31 +0700

    #50 fixing command input

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -39,7 +39,8 @@ public class EchoController {
         } else if (command.equals("/is_airing")) {
             AnimeBot ab = new AnimeBot(input);
             return new TextMessage(ab.getStatusMessage());
-        } else if ((command.equals("hari ini nonton apa?"))) {
+        } else if ((splitContent.equals("hari ini nonton apa?"))
+                && (event.getSource() instanceof GroupSource)) {
             ListAnimeBot lab = new ListAnimeBot();
             return new TextMessage(lab.getListAnime());
         }
