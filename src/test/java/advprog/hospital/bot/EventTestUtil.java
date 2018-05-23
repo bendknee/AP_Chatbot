53eeea7a94bcb8cb3781771f0facf5e693ac56b8
bagasirwansyah <bagasirwansyah@yahoo.com>
2018-05-23 07:32:46 +0700

    Improving checkstyle

--- src/test/java/advprog/hospital/bot/EventTestUtil.java
+++ src/test/java/advprog/hospital/bot/EventTestUtil.java
@@ -19,7 +19,8 @@ public class EventTestUtil {
                 Instant.parse("2018-01-01T00:00:00.000Z"));
     }
 
-    public static MessageEvent<LocationMessageContent> createDummyLocationMessage(double latitude, double longitude) {
+    public static MessageEvent<LocationMessageContent> createDummyLocationMessage
+            (double latitude, double longitude) {
         return new MessageEvent<>("replyToken", new UserSource("userId"),
                 new LocationMessageContent("id", "title","address",
                         latitude, longitude),
